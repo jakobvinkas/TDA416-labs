ae01b50c9cd10798756f9cd7d7faf78e2045665f
Mats Högberg <mats@hgbrg.se>
2016-01-28 14:25:58 +0100

    Set up code for iterating through points

--- Lab2/Lab2a.java
+++ Lab2/Lab2a.java
@@ -1,16 +1,41 @@
 public class Lab2a {
-  public static double[] simplifyShape(double[] poly, int k)
-  {
-      // TODO
-  }
+    public static double[] simplifyShape(double[] poly, int k) {
+        while (poly.length > k) {
+            double minImportance = -1;
+            int minImportanceIndex = -1;
 
+            // find least important point
+            for (int i = 2; i < poly.length - 2; i += 2) {
+                double importance = calcImportance(poly[i - 2], poly[i - 1],
+                        poly[i], poly[i + 1], poly[i + 2], poly[i + 3]);
+                if (minImportance == -1 || importance < minImportance) {
+                    minImportance = importance;
+                    minImportanceIndex = i;
+                }
+            }
 
-  private static double calcImportance(double x1, double y1, double x2, double y2, double x3, double y3){
-    double l1 = calcDist(x1, y1, x2, y2);
-    double l2 = calcDist(x2, y2, x3, y3);
-    double l3 = calcDist(x1, y1, x3, y3);
-    return l1+l2-l3;
-  }
+            // remove least important point from array
+            double[] tmp = new double[poly.length - 2];
+            int tmpIndex = 0;
+            for (int i = 0; i < poly.length; i += 2) {
+                if (i != minImportanceIndex) {
+                    tmp[tmpIndex] = poly[i];
+                    tmp[tmpIndex + 1] = poly[i + 1];
+                    tmpIndex++;
+                }
+            }
+            poly = tmp;
+        }
+
+        return poly;
+    }
+
+    private static double calcImportance(double x1, double y1, double x2, double y2, double x3, double y3){
+        double l1 = calcDist(x1, y1, x2, y2);
+        double l2 = calcDist(x2, y2, x3, y3);
+        double l3 = calcDist(x1, y1, x3, y3);
+        return l1+l2-l3;
+    }
 
 
     /**
@@ -21,9 +46,9 @@ public class Lab2a {
      * @param y2
      * @return
      */
-  private static double calcDist(double x1, double y1, double x2, double y2){
-    double distX = Math.abs(x1-x2);
-    double distY = Math.abs(y1-y2);
-    return Math.sqrt(Math.pow(distX, 2)+Math.pow(distY, 2));
-  }
+    private static double calcDist(double x1, double y1, double x2, double y2) {
+        double distX = Math.abs(x1-x2);
+        double distY = Math.abs(y1-y2);
+        return Math.sqrt(Math.pow(distX, 2)+Math.pow(distY, 2));
+    }
 }
--- Lab2/Lab2b.java
+++ Lab2/Lab2b.java
@@ -2,8 +2,7 @@ import java.util.PriorityQueue;
 
 
 public class Lab2b {
-  public static double[] simplifyShape(double[] poly, int k)
-  {
-      // TODO
-  }
+    public static double[] simplifyShape(double[] poly, int k) {
+        return poly;
+    }
 }
