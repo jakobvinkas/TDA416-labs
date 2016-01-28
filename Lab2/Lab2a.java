d50885c12a0d6b10d24da68b505d122b6d48f76c
Maxim Goretskyy <13maxim13@live.se>
2016-01-28 14:25:36 +0100

    Adds helper methods to calc importance of a point

--- Lab2/Lab2a.java
+++ Lab2/Lab2a.java
@@ -3,4 +3,27 @@ public class Lab2a {
   {
       // TODO
   }
+
+
+  private static double calcImportance(double x1, double y1, double x2, double y2, double x3, double y3){
+    double l1 = calcDist(x1, y1, x2, y2);
+    double l2 = calcDist(x2, y2, x3, y3);
+    double l3 = calcDist(x1, y1, x3, y3);
+    return l1+l2-l3;
+  }
+
+
+    /**
+     * Uses pythagorean theorem
+     * @param x1
+     * @param y1
+     * @param x2
+     * @param y2
+     * @return
+     */
+  private static double calcDist(double x1, double y1, double x2, double y2){
+    double distX = Math.abs(x1-x2);
+    double distY = Math.abs(y1-y2);
+    return Math.sqrt(Math.pow(distX, 2)+Math.pow(distY, 2));
+  }
 }
