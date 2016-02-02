43bfcf0137938647f00700482195e8f449d010b1
Maxim Goretskyy <13maxim13@live.se>
2016-02-02 11:17:32 +0100

    Adds algorithm

--- Lab2/Lab2b.java
+++ Lab2/Lab2b.java
@@ -2,7 +2,46 @@ import java.util.PriorityQueue;
 
 public class Lab2b {
     public static double[] simplifyShape(double[] poly, int k) {
-        return new double[k];
+        PriorityQueue<Node<Point>> queue = new PriorityQueue<>();
+        DLList<Point> points = new DLList<>();
+        Point previous = new Point(poly[0], poly[1]);
+        Point current = new Point(poly[2], poly[3]);
+        DLList.Node<Point> currNode = points.addFirst(previous);
+        currNode = points.insertAfter(current, currNode);
+        for (int i = 4; i<poly.length; i+=2 ) {
+            Point next = new Point(poly[i], poly[i+1]);
+            current.setImportance(previous, next);
+            queue.add(currNode);
+            currNode = points.insertAfter(next, currNode);
+            previous = current;
+            current = next;
+        }
+
+        while (queue.size() > k) {
+            DLList.Node<Point> removed = queue.remove();
+            DLList.Node<Point> prev  = removed.prev;
+            DLList.Node<Point> next = removed.next;
+            points.remove(removed);
+            prev.getValue().setImportance(prev.prev.getValue(), next.getValue());
+            next.getValue().setImportance(prev.getValue(), next.next.getValue());
+            queue.remove(prev);
+            queue.remove(next);
+            queue.add(prev);
+            queue.add(next);
+        }
+        ArrayList<Double> newPoly = new ArrayList<>();
+
+        while(points.getFirst() != null) {
+            DLList.Node<Point> p = points.getFirst();
+            newPoly.add(p.getValue().getX());
+            newPoly.add(p.getValue().getY());
+            points.remove(p);
+        } 
+        return newPoly.toArray();     
+    }
+
+    private static void updateNodes(DLList.Node<Point> node) {
+
     }
 }
 
@@ -14,6 +53,7 @@ class Point implements Comparable<Point> {
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
+        this.importance = 0;
     }
 
     public double getX() {
