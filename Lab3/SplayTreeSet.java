f8e8182217f9dff335e3be9a8263c5d4289386ba
Mats Högberg <mats@hgbrg.se>
2016-02-11 16:17:05 +0100

    Added zig and zag

--- Lab3/SplayTreeSet.java
+++ Lab3/SplayTreeSet.java
@@ -37,4 +37,16 @@ public class SplayTreeSet<E extends Comparable<? super E>> implements SimpleSet<
     public boolean contains(E x) {
         return false;
     }
+
+    private void zig(Node node) {
+        root.left = node.right;
+        node.right = root;
+        root = node;
+    }
+
+    private void zag(Node node) {
+        root.right = node.left;
+        node.left = root;
+        root = node;
+    }
 }
