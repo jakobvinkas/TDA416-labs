ad4efea4157d35363a8778f616cec87a8a6c580e
Mats Högberg <mats@hgbrg.se>
2016-02-11 15:44:10 +0100

    Boilerplate code for SplayTreeSet

---
+++ Lab3/SplayTreeSet.java
@@ -0,0 +1,17 @@
+public class SplayTreeSet implements SimpleSet {
+    public int size() {
+        return 0;
+    }
+
+    public boolean add(E x) {
+        return false;
+    }
+
+    public boolean remove(E x) {
+        return false;
+    }
+
+    public boolean contains(E x) {
+        return false;
+    }
+}
