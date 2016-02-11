b7e653059748273c4603e53f8d5baa4c7dc762e3
Mats Högberg <mats@hgbrg.se>
2016-02-11 16:16:44 +0100

    Added implementation of node

--- Lab3/SplayTreeSet.java
+++ Lab3/SplayTreeSet.java
@@ -1,4 +1,27 @@
 public class SplayTreeSet<E extends Comparable<? super E>> implements SimpleSet<E> {
+    class Node {
+        private E value;
+        protected Node parent;
+        protected Node leftChild;
+        protected Node rightChild;
+
+        public Node(E value) {
+            this.value = value;
+        }
+
+        public E getValue() {
+            return this.value;
+        }
+    }
+
+    Node root;
+    int size;
+
+    public SplayTreeSet() {
+        this.root = null;
+        this.size = 0;
+    }
+
     public int size() {
         return 0;
     }
