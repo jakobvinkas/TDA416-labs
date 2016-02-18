c5e0f5c84ad964a08f22ef8fd5812298fa92192e
Mats Högberg <mats@hgbrg.se>
2016-02-18 15:39:59 +0100

    Reimplemented zig and zag

--- Lab3/SplayTreeSet.java
+++ Lab3/SplayTreeSet.java
@@ -143,24 +143,20 @@ public class SplayTreeSet<E extends Comparable<? super E>> implements SimpleSet<
         }
     }
 
+    private void zig(Node node) {
+        this.rotateRight(node);
+    }
+
+    private void zag(Node node) {
+        this.rotateLeft(node);
+    }
+
     public boolean remove(E x) {
         return false;
     }
 
     public boolean contains(E x) {
         return false;
-    }
-
-    private void zig(Node node) {
-        root.left = node.right;
-        node.right = root;
-        root = node;
-    }
-
-    private void zag(Node node) {
-        root.right = node.left;
-        node.left = root;
-        root = node;
     }
 
     public String toString() {
