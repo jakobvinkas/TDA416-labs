a92dcc78d7903d58fa368beca0e38ef0944343fb
Mats Högberg <mats@hgbrg.se>
2016-02-25 16:07:23 +0100

    Fist implementation of a directed graph

--- Lab4/DirectedGraph.java
+++ Lab4/DirectedGraph.java
@@ -2,23 +2,24 @@
 import java.util.*;
 
 public class DirectedGraph<E extends Edge> {
-
+	private List<E>[] nodes;
 
 	public DirectedGraph(int noOfNodes) {
-		;
+		nodes = new List[noOfNodes];
+		for (int i = 0; i < noOfNodes; i++) {
+			nodes[i] = new LinkedList<>();
+		}
 	}
 
 	public void addEdge(E e) {
-		;
+		nodes[e.getSource()].add(e);
 	}
 
 	public Iterator<E> shortestPath(int from, int to) {
 		return null;
 	}
-		
+
 	public Iterator<E> minimumSpanningTree() {
 		return null;
 	}
-
 }
-  
